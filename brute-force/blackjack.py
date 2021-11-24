3e6f1aba775d7e55d4eb58f0d7b2aa2a8720e36e
Alparang <evering6035@naver.com>
2021-11-25 03:24:50 +0900

    blackjack 001

--- brute-force/blackjack.py
+++ brute-force/blackjack.py
@@ -4,20 +4,18 @@ card_list = list(map(int, input().split()))
 
 #결과값
 result = 0
-tmp = 0
+#임시값
+tmp_value = 0
 
+
+# (0~n), (i+1~n), (j+1~n) 으로 중복 없이 3개씩 뽑는다.
 for i in range(n):
-    for j in range(n):
-        if(i != j):
-            tmp = card_list[i] + card_list[j]
-        else:
-            continue
-        for k in range(n):
-            if(k != i  and k != j):
-                tmp += card_list[k]
-                if(tmp <= m and tmp >= result):
-                    result = tmp
-        else:
-             continue             
+    for j in range(i+1, n):
+        for k in range (j+1, n):
+            tmp_value = card_list[i] + card_list[j] + card_list[k]
+            if(tmp_value <= m):
+                result = max(result, tmp_value)
+            else:
+                continue
 
 print(result)
