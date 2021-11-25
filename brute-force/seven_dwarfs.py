5008d6ba74a7188b8dbc89fd9b8ffc62f02ef4e7
Alparang <evering6035@naver.com>
2021-11-26 03:22:29 +0900

    seven_dwarfs ok

--- brute-force/seven_dwarfs.py
+++ brute-force/seven_dwarfs.py
@@ -5,18 +5,22 @@ for i in range(9):
     height_list.append(int(input()))
 
 total = sum(height_list)
-#print("total : ")
-#print(total)
 
 for p in range(8):
-    for q in range(i + 1, 9):
-        if((total - height_list[p] - height_list[q]) == 100):
-            height_list.remove(height_list[p])
-            height_list.remove(height_list[q])
+    for q in range(p + 1, 9):
+        if((total - (height_list[p] + height_list[q])) == 100):
+            rem_1 = height_list[p]
+            rem_2 = height_list[q]
+
+            height_list.remove(rem_1)
+            height_list.remove(rem_2)
+
+            height_list.sort()
+
+            print(p, q)
+            for k in range(7):
+                print(height_list[k])
             break
-
-height_list = sorted(height_list)
-#print(height_list)
-
-for k in range(7):
-    print(height_list[k])
+    
+    if(len(height_list) < 9):
+        break
