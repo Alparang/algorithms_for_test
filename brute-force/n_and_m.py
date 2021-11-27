3a7b64ceff0cfa3dd08c80de8974966babb1fb6d
Alparang <evering6035@naver.com>
2021-11-27 15:28:08 +0900

    n and m ok

--- brute-force/n_and_m.py
+++ brute-force/n_and_m.py
@@ -1,14 +1,17 @@
 n, m = map(int, input().split())
-result = []
-tmp = [i for i in range(n)]
+visited = [False] * n # 탐사 노드
+result = [] # 출력
 
-for i in 
-
-
-
-print(result)
-
-# while(m):
-    
-
-#     m = m - 1
+def solve(depth, n, m):
+    if depth == m:
+        print(' '.join(map(str, result))) #결과를 띄어쓰기 하여 출력
+        return
+    for i in range(len(visited)):
+        if visited[i] == False:
+            visited[i] = True
+            result.append(i+1)
+            solve(depth + 1, n, m) #dfs
+            visited[i] = False
+            result.pop()
+        
+solve(0, n, m)
