fddb27a0e1b76eb9679101590ec782ef3281088d
Alparang <evering6035@naver.com>
2021-11-27 17:42:20 +0900

    1,2,3더하기ok

--- 1주차/1,2,3더하기.py
+++ 1주차/1,2,3더하기.py
@@ -1,7 +1,13 @@
 n = int(input()) #테스트 케이스 개수
 test_list = []  # 테스트 리스트
-
 for i in range(n):
+
     test_list.append(int(input()))
 
-print(test_list)
+dp = [1, 2, 4] #1,2,4 for 점화식
+
+for i in range(3, 10):
+    dp.append(dp[i-1] + dp[i-2] + dp[i-3])
+
+for i in test_list:
+    print(dp[i-1])
