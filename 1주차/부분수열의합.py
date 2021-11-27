a6f1297ebbad8b0d21b8a2da023f191457b7398c
Alparang <evering6035@naver.com>
2021-11-27 18:41:52 +0900

    부분수열의합 ok

--- 1주차/부분수열의합.py
+++ 1주차/부분수열의합.py
@@ -1,1 +1,17 @@
 n,s = map(int, input().split())
+data = list(map(int, input().split()))
+data_len = len(data)
+count = 0
+
+def dfs(idx ,sum):
+    global count
+    if(idx >= n):
+        return 
+    sum += data[idx]
+    if sum == s:
+        count += 1
+    dfs(idx+1, sum)                 # 현재 data 값을 더하는 경우
+    dfs(idx+1, sum-data[idx])       # 현재 data 값을 더하지 않는 경우
+
+dfs(0,0)
+print(count)
